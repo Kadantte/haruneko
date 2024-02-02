b50320e80ef49f61fc7f80b395ece6a701c08ff6
Ronny <wegener.ronny@gmail.com>
2024-02-02 17:58:24 +0100

    nw with vite

--- app/nw/vite.config.ts
+++ app/nw/vite.config.ts
@@ -1,24 +1,14 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
-import pkg from './package.json' assert { type: 'json' };
 
 export default defineConfig({
     build: {
         ssr: true,
-        outDir: 'build',
         emptyOutDir: false,
-        target: 'esnext',
+        outDir: resolve(__dirname, 'build'),
         lib: {
             entry: resolve(__dirname, 'src', 'App.ts'),
-            name: 'hakuneko-app-nw',
-            //formats: [ 'cjs' ]
-        },
-        rollupOptions: {
-            //input: resolve(__dirname, 'src', 'App.ts'),
-            external: [
-                ...Object.keys(pkg.dependencies), // don't bundle dependencies
-                /^node:.*/, // don't bundle built-in Node.js modules (use protocol imports!)
-            ],
+            formats: [ 'cjs' ]
         },
     },
 });
