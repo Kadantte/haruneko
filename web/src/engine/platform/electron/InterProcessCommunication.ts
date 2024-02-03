d79c8315cdaf9284b9a496cec1ea40ad96023a76
Ronny <wegener.ronny@gmail.com>
2024-02-03 17:00:16 +0100

    wip

--- web/src/engine/platform/electron/InterProcessCommunication.ts
+++ web/src/engine/platform/electron/InterProcessCommunication.ts
@@ -9,25 +9,25 @@ export default class implements PlatformIPC {
     constructor(private readonly settingsManager: SettingsManager) {}
 
     public async StopRPC(): Promise<void> {
-        console.debug('Web::IPC::StopRPC');
+        console.debug('Web::IPC::StopRPC()');
         // => Sent to Main
         throw new Error('Method not implemented.');
     }
 
     public async RestartRPC(port: number, secret: string): Promise<void> {
-        console.debug('Web::IPC::', port, secret);
+        console.debug('Web::IPC::RestartRPC()', '=>', port, secret);
         // => Send to Main
         throw new Error('Method not implemented.');
     }
 
     public async SetCloudFlareBypass(userAgent: string, cookies: TypeFromInterface<chrome.cookies.Cookie>[]): Promise<void> {
-        console.debug('Web::IPC::SetCloudFlareBypass', userAgent, cookies);
+        console.debug('Web::IPC::SetCloudFlareBypass()', '=>', userAgent, cookies);
         // => Receive on Renderer
         throw new Error('Method not implemented.');
     }
 
     public async LoadMediaContainerFromURL(url: string): Promise<void> {
-        console.debug('Web::IPC::LoadMediaContainerFromURL', url);
+        console.debug('Web::IPC::LoadMediaContainerFromURL()', '=>', url);
         // => Receive on Renderer
         throw new Error('Method not implemented.');
     }
